fea06b36eaf0e00d5a0e1f3433a33451fc8f356c
joelmiller <joel.c.miller@gmail.com>
2017-04-28 21:55:36 -0700

    Revert "add sphinx.ext.autosummary to conf.py"
    This reverts commit 26e96af3c849e5373655070949a41622f767d606.

--- docs/conf.py
+++ docs/conf.py
@@ -34,8 +34,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary']
+    'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
